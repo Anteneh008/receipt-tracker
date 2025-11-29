c6247e17a7b058594e97ad2c3a67a9197a938edf
Anteneh Atre <anteneh.atre@gmail.com>
2025-11-29 12:14:52 +0300

    Update package dependencies, enhance global styles, and improve Header component with user authentication features

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Link from "next/link";
+import { Shield } from "lucide-react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 function Header() {
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
   return (
-    <div>Header</div>
-  )
+    <div
+      className={`p-4 flex justify-between ${isHomePage ? "bg-blue-50" : "bg-white border-b border-blue-50"}`}
+    >
+      <Link href="/" className="flex items-center">
+        <Shield className="w-6 h-6 text-blue-600 mr-2" />
+        <h1>Expensio</h1>
+      </Link>
+
+      <div className="flex items-center space-x-4">
+        <SignedIn>
+          <Link href="/receipts">
+            <Button variant="outline">My Receipts</Button>
+          </Link>
+
+          <Link href="/manage-plan">
+            <Button>Manage Plan</Button>
+          </Link>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
