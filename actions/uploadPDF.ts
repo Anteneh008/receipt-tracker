b0495986a13674820d8288eb170be8f4c87265dc
Anteneh Atre <anteneh.atre@gmail.com>
2025-12-02 18:42:59 +0300

    Integrate Inngest functionality into PDF upload process by triggering an agent flow to extract data from uploaded PDFs and save it to the database.

--- actions/uploadPDF.ts
+++ actions/uploadPDF.ts
@@ -4,6 +4,8 @@ import { api } from "@/convex/_generated/api";
 import convex from "@/lib/convexClient";
 import { currentUser } from "@clerk/nextjs/server";
 import { getFileDownloadUrl } from "./getFileDownloadUrl";
+import { inngest } from "@/inngest/client";
+import Events from "@/inngest/constants";
 
 // Server action to upload a PDF file to Convex Storage
 
@@ -65,7 +67,13 @@ export async function uploadPDF(formData: FormData) {
     const fileUrl = await getFileDownloadUrl(storageId);
 
     // TODO: Triger inngest agent flow
-    // ...
+    await inngest.send({
+      name: Events.EXTRACT_DATA_FROM_PDF_AND_SAVE_TO_DATABASE,
+      data: {
+        url: fileUrl.downloadUrl,
+        receiptId,
+      },
+    });
 
     return {
       success: true,
